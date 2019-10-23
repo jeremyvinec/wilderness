434a31859aa2911bba51e509aaf981c520854ff1
jeremyvinec <jeremy.yvinec@gmail.com>
2019-10-23 09:57:16 +0200

    userlocation

--- src/components/screens/Map.tsx
+++ src/components/screens/Map.tsx
@@ -1,22 +1,46 @@
 import MapboxGL from '@react-native-mapbox-gl/maps'
-import React, { Component } from 'react'
-// import SetUserTrackingModes from './SetUserTrackingModes'
-import { StyleSheet } from 'react-native'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
 import config from '../../utils/config.js'
+import Bubble from '../common/Bubble'
 
 MapboxGL.setAccessToken(config.get('accessToken'))
-export default class Map extends Component {
+
+interface State {
+  showUserLocation: boolean,
+}
+
+interface Props { }
+export default class Map extends React.Component<State, Props>{
+
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      showUserLocation: true,
+
+    }
+  }
+
+  onToggleUserLocation() {
+    this.setState({showUserLocation: !this.state.showUserLocation});
+  }
+
   render() {
+    const { showUserLocation } = this.state
     return (
-      <MapboxGL.MapView
-        style={styles.map}
-        styleURL={MapboxGL.StyleURL.Outdoors}
-      >
-         <MapboxGL.Camera
-            centerCoordinate={[6.075870, 44.559860]}
-            zoomLevel={12}
-         />
-      </MapboxGL.MapView>
+      <View style={styles.map}>
+        <MapboxGL.MapView
+          style={styles.map}
+          styleURL={MapboxGL.StyleURL.Outdoors}
+        >
+          <MapboxGL.UserLocation visible={showUserLocation}/>
+          <MapboxGL.Camera
+              centerCoordinate={[6.075870, 44.559860]}
+              zoomLevel={12}
+          />
+        </MapboxGL.MapView>
+        <Bubble onPress={this.onToggleUserLocation} style={{bottom: 180}}/>
+      </View>
     )
   }
 }
